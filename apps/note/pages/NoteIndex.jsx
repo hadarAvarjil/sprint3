41c9b6a750b48a609caac8be70a4156dac008dad
Haimmichaeli90 <haimmichaeli95@gmail.com>
2024-10-03 11:41:44 +0300

    create NoteIndex to manage notes

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,18 @@
+import { NoteList } from "../NoteList.jsx"
+import { noteService } from "../../../services/note.service.js"
+import React, { useEffect, useState } from "react"
 
 export function NoteIndex() {
-    return <div>note app</div>
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        noteService.query().then(setNotes)
+    }, [])
+
+    return (
+        <div className="note-index">
+            <h1>Your Notes</h1>
+            <NoteList notes={notes} />
+        </div>
+    );
 }
