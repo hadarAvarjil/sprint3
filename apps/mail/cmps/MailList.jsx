3217750b608ee88186fa8844d22c1112918ed692
hadar Avarjil <hadaravarjil@gmail.com>
2024-10-04 14:14:18 +0300

    add css for MailList component

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -5,18 +5,27 @@ import { MailPreview } from "./MailPreview.jsx";
 export function MailList({ mails, onRemoveMail }) {
 
     return (
-        <ul className="mail-list">
-            {mails.map(mail =>
-                <li key={mail.id}>
-                    <MailPreview mail={mail} />
-                    <section>
-                        <button onClick={() => onRemoveMail(mail.id)}>Remove</button>
-                        <button ><Link to={`/mail/${mail.id}`}>Details</Link></button>
-                        <button ><Link to={`/mail/edit/${mail.id}`}>Edit</Link></button>
-                    </section>
-                </li>
-            )}
-        </ul>
+        < table className="mail-list">
+            <tbody>
+                {mails.map(mail =>
+                    <tr key={mail.id}>
+                        <td>
+                            <i className={mail.isStarred ? "icon-starred" : "icon-unstarred"}></i>
+                        </td>
+                        <td>
+                            <MailPreview mail={mail} />
+                        </td>
+                        <td>
+                            <section className="mail-actions">
+                                <button onClick={() => onRemoveMail(mail.id)}>Remove</button>
+                                <button><Link to={`/mail/${mail.id}`}>Details</Link></button>
+                                <button><Link to={`/mail/edit/${mail.id}`}>Edit</Link></button>
+                            </section>
+                        </td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
     )
 
 }
--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -4,7 +4,19 @@ export function MailPreview({ mail }) {
 
     return (
         <article className="mail-preview">
-            <h2>Vendor: {mail.vendor}</h2>
+            <h3>{mail.from}</h3>
+            <h3>{mail.subject}</h3>
+            <h3>{mail.createdAt}</h3>
         </article>
     )
 }
+
+// id: utilService.makeId(),
+// createdAt : 1551133930500,
+// subject: utilService.makeLorem(4),
+// body: utilService.makeLorem(20),
+// isRead:  Math.random() > 0.7,
+// sentAt : 1551133930594,
+// removedAt : null,
+// from: 'momo@momo.com',
+// to: 'user@appsus.com'
