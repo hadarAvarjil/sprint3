5c7abed0961886ea628fcf0d95f47c212fe8fce6
Haimmichaeli90 <haimmichaeli95@gmail.com>
2024-10-05 00:09:06 +0300

    add css with new class for input fields

--- apps/note/cmps/NoteForm.jsx
+++ apps/note/cmps/NoteForm.jsx
@@ -50,65 +50,66 @@ export function NoteForm({ onSave, existingNote }) {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="note-form">
+        <input
+            type="text"
+            name="title"
+            value={note.info.title}
+            onChange={handleChange}
+            placeholder="Note Title" 
+        />
+        <select name="type" value={note.type} onChange={(e) => setNote({ ...note, type: e.target.value })}>
+            <option value="NoteTxt">Text Note</option>
+            <option value="NoteImg">Image Note</option>
+            <option value="NoteVideo">Video Note</option>
+            <option value="NoteTodos">Todo List Note</option>
+        </select>
+        {note.type === 'NoteTxt' && (
             <input
                 type="text"
-                name="title"
-                value={note.info.title}
+                name="txt"
+                value={note.info.txt}
                 onChange={handleChange}
-                placeholder="Note Title" 
+                placeholder="Note Content"
             />
-            <select name="type" value={note.type} onChange={(e) => setNote({ ...note, type: e.target.value })}>
-                <option value="NoteTxt">Text Note</option>
-                <option value="NoteImg">Image Note</option>
-                <option value="NoteVideo">Video Note</option>
-                <option value="NoteTodos">Todo List Note</option>
-            </select>
-            {note.type === 'NoteTxt' && (
-                <input
-                    type="text"
-                    name="txt"
-                    value={note.info.txt}
-                    onChange={handleChange}
-                    placeholder="Note Content"
-                />
-            )}
-            {note.type === 'NoteImg' && (
-                <input
-                    type="text"
-                    name="url"
-                    value={note.info.url}
-                    onChange={handleChange}
-                    placeholder="Image URL"
-                />
-            )}
-            {note.type === 'NoteVideo' && (
-                <input
-                    type="text"
-                    name="url"
-                    value={note.info.url}
-                    onChange={handleChange}
-                    placeholder="Video URL"
-                />
-            )}
-            {note.type === 'NoteTodos' && (
-                <div>
-                    {note.info.todos && note.info.todos.length > 0 ? (
-                        note.info.todos.map((todo, index) => (
-                            <div key={index}>
-                                <input
-                                    type="text"
-                                    value={todo.txt}
-                                    onChange={(e) => handleTodoChange(index, e.target.value)}
-                                    placeholder={`Todo ${index + 1}`}
-                                />
-                            </div>
-                        ))
-                    ) : null}
-                    <button type="button" onClick={handleAddTodo}>Add Todo</button>
-                </div>
-            )}
-            <button type="submit">{existingNote ? 'Update Note' : 'Add Note'}</button>
-        </form>
+        )}
+        {note.type === 'NoteImg' && (
+            <input
+                type="text"
+                name="url"
+                value={note.info.url}
+                onChange={handleChange}
+                placeholder="Image URL"
+            />
+        )}
+        {note.type === 'NoteVideo' && (
+            <input
+                type="text"
+                name="url"
+                value={note.info.url}
+                onChange={handleChange}
+                placeholder="Video URL"
+            />
+        )}
+        {note.type === 'NoteTodos' && (
+            <div>
+                {note.info.todos && note.info.todos.length > 0 ? (
+                    note.info.todos.map((todo, index) => (
+                        <div key={index} className="todo-item">
+                            <input
+                                type="text"
+                                value={todo.txt}
+                                onChange={(e) => handleTodoChange(index, e.target.value)}
+                                placeholder={`Todo ${index + 1}`}
+                            />
+                        </div>
+                    ))
+                ) : null}
+                <button type="button" onClick={handleAddTodo} className="add-todo-button">Add Todo</button>
+            </div>
+        )}
+        <button type="submit">{existingNote ? 'Update Note' : 'Add Note'}</button>
+    </form>
+
     )
 }
