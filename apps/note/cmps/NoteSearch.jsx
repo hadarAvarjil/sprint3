3abaff9d562e60a52e702fb29bcbafd6ed7d2ed9
hadar Avarjil <hadaravarjil@gmail.com>
2024-10-09 21:00:45 +0300

    add css for note

--- apps/note/cmps/NoteSearch.jsx
+++ apps/note/cmps/NoteSearch.jsx
@@ -1,21 +1,23 @@
 export function SearchBar({ searchTerm, onSearchChange }) {
     return (
         <section className="note-filter">
-        <div className="keep-header">
-            <div className="keep-logo-header">
-                <span className="material-icons">menu</span>
-                <img className="keep-logo" src="assets/img/keep_google_logo.png" alt="keep-logo" />
-                Keep
+            <div className="keep-header">
+                <div className="keep-logo-header">
+                    <span className="material-icons">menu</span>
+                    <img className="keep-logo" src="assets/img/keep_google_logo.png" alt="keep-logo" />
+                    Keep
+                </div>
+
+                <div className="search-bar-container">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        value={searchTerm}
+                        onChange={onSearchChange}
+                        className="search-bar"
+                    />
+                </div>
             </div>
-        <input
-            type="text"
-            placeholder="Search notes..."
-            value={searchTerm}
-            onChange={onSearchChange}
-            className="search-bar"
-        />
-            </div>
-       
         </section>
     )
 }
