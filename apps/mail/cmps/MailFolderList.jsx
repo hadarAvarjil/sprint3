c1ea39012ecb9fca72f1b1c4b78f7140ed7ed89e
hadar Avarjil <hadaravarjil@gmail.com>
2024-10-09 19:02:53 +0300

    outlet remove

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -68,7 +68,7 @@ export function MailFolderList({ onSetFilterBy }) {
                         </Link>
                     </li>
                 </ul>
-            <Outlet />
+            {/* <Outlet /> */}
         </aside>
     )
 }
