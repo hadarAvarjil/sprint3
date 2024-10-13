6463f3d92cbe4c4904c79560a386a85c47bf81de
Haimmichaeli90 <haimmichaeli95@gmail.com>
2024-10-13 18:22:02 +0300

    create home page + buttons

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,15 @@
+const { Link } = ReactRouterDOM
 
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
+  return (
+    <section className="home-view">
+      <div className="hero">
+        <h1>Welcome to AppSus !</h1>
+        <p>We are here to provide you with top-notch applications.</p>
+        <Link to="/mail"><button className="btn">Mail</button></Link>
+        <Link to="/note"><button className="btn">Notes</button></Link>
+        <Link to="/book"><button className="btn">Books</button></Link>
+      </div>
     </section>
+  );
 }
