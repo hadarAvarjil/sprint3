9b258f184b3aa8eace6c95bfde62023d728273b4
hadar Avarjil <hadaravarjil@gmail.com>
2024-10-14 18:29:42 +0300

    home

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,18 +1,18 @@
 export function About() {
     return (
-      <section class="about-view">
+      <section className="about-view">
       <h1>About Us</h1>
   
-      <section class="content">
+      <section className="content">
           <p>This website was developed by Hadar Avarjil & Haim Michaeli.</p>
   
-          <div class="teammate">
+          <div className="teammate">
               <h4>Hadar Avarjil</h4>
               <p>Email: <a href="mailto:hadaravarjil@gmail.com">hadaravarjil@gmail.com</a></p>
               <p>Github: <a href="https://github.com/hadaravarjil" target="_blank">hadaravarjil</a></p>
           </div>
   
-          <div class="teammate">
+          <div className="teammate">
               <h4>Haim Michaeli</h4>
               <p>Email: <a href="mailto:haim.hmichael@gmail.com">haim.hmichael@gmail.com</a></p>
               <p>Github: <a href="https://github.com/HaimMichaeli90" target="_blank">HaimMichaeli90</a></p>
